057326a292c2e14f811d5058312c0a9769705224
Aleksey Razbakov <aleksey@razbakov.com>
2020-05-23 19:51:41 +0200

    fix popup menu

--- components/TMenu.vue
+++ components/TMenu.vue
@@ -5,7 +5,7 @@
     </div>
     <div
       v-if="show"
-      class="absolute border right-0 text-left -mt-1 w-32 py-2 bg-white rounded-lg shadow-xl"
+      class="absolute border right-0 text-left -mt-1 w-32 py-2 bg-white rounded-lg shadow-xl z-30"
     >
       <slot name="menu" :closeMenu="closeMenu" />
     </div>
