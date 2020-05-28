708eca924baa48951ce57aa90ac9b950d560b858
Aleksey Razbakov <aleksey@razbakov.com>
2020-05-28 13:53:16 +0200

    fix textarea

--- components/TInput/TInputTextarea.vue
+++ components/TInput/TInputTextarea.vue
@@ -1,7 +1,7 @@
 <template>
   <textarea
-    v-bind="$attrs"
     v-model="input"
+    v-bind="$attrs"
     class="w-full block bg-gray-200 appearance-none font-mono border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
   />
 </template>
