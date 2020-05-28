ff39d395feb8f309142c6c8e4be2e06fc8dabbca
Aleksey Razbakov <aleksey@razbakov.com>
2020-05-28 13:52:48 +0200

    fix textarea

--- components/TInput/TInputTextarea.vue
+++ components/TInput/TInputTextarea.vue
@@ -1,9 +1,8 @@
 <template>
   <textarea
     v-bind="$attrs"
-    :value.sync="value"
+    v-model="input"
     class="w-full block bg-gray-200 appearance-none font-mono border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-    @input="(val) => $emit('input', val)"
   />
 </template>
 
@@ -13,6 +12,16 @@ export default {
     value: {
       type: String,
       default: ''
+    }
+  },
+  computed: {
+    input: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
     }
   }
 }
