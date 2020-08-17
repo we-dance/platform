71a432d01dc92659430d576836f5a94b13c3b9bf
Aleksey Razbakov <aleksey@razbakov.com>
2020-08-18 00:54:32 +0200

    add visuals

--- pages/_city.vue
+++ pages/_city.vue
@@ -17,38 +17,43 @@
       </div>
     </header>
 
-    <div class="bg-orange-100">
-      <div class="text-gray-900 py-20 px-4 mx-auto max-w-2xl">
-        <h1 class="text-4xl font-serif leading-tight">
-          Dance community in
-          <div class="inline-block">
-            <TInputSelect
-              :options="cityList"
-              :value="citySelected"
-              @input="(city) => $router.push(`/${city}`)"
-            />
+    <div class="bg-hero">
+      <div class="bg-black bg-opacity-75">
+        <div class="text-white py-40 px-4 mx-auto max-w-2xl">
+          <h1 class="text-4xl font-serif leading-tight">
+            Dance community in
+            <div class="inline-block text-black">
+              <TInputSelect
+                :options="cityList"
+                :value="citySelected"
+                @input="(city) => $router.push(`/${city}`)"
+              />
+            </div>
+          </h1>
+          <p class="mt-2 text-xl leading-tight">
+            Connect with dancers and plan activities easier. Salsa, Bachata,
+            Kizomba, Zouk and more!
+          </p>
+          <div class="md:flex text-center">
+            <TButton class="mt-4 md:mr-4" type="primary" to="/signin"
+              >Join now</TButton
+            >
+            <TButton
+              class="mt-4"
+              type="secondary"
+              href="https://www.notion.so/WeDance-d8a81e56732e4c10958073a8b887a673"
+              >About WeDance</TButton
+            >
           </div>
-        </h1>
-        <p class="mt-2 text-xl leading-tight">
-          Connect with dancers and plan activities easier. Salsa, Bachata,
-          Kizomba, Zouk and more!
-        </p>
-        <div class="md:flex text-center">
-          <TButton class="mt-4 md:mr-4" type="primary" to="/signin"
-            >Join community</TButton
-          >
-          <TButton
-            class="mt-4"
-            type="secondary"
-            href="https://www.notion.so/WeDance-d8a81e56732e4c10958073a8b887a673"
-            >About WeDance</TButton
-          >
         </div>
       </div>
     </div>
 
     <div class="p-4 max-w-2xl mx-auto">
       <div class="mt-4 flex min-w-full justify-end">
+        <div class="w-1/3">
+          <TIcon name="undraw_i_can_fly" class="pr-4" />
+        </div>
         <div class="w-2/3 h-40">
           <h2 class="font-bold text-xl">Start dancing</h2>
           <p>
@@ -65,8 +70,14 @@
             someone.
           </p>
         </div>
+        <div class="w-1/3">
+          <TIcon name="undraw_together" class="pl-4" />
+        </div>
       </div>
       <div class="mt-4 flex min-w-full justify-end">
+        <div class="w-1/3">
+          <TIcon name="undraw_events" class="pr-4" />
+        </div>
         <div class="w-2/3 h-40">
           <h2 class="font-bold text-xl">Find parties</h2>
           <p>
@@ -83,9 +94,15 @@
             festivals.
           </p>
         </div>
+        <div class="w-1/3">
+          <TIcon name="undraw_super_woman" class="pl-4" />
+        </div>
       </div>
 
       <div class="mt-4 flex min-w-full justify-end">
+        <div class="w-1/3">
+          <TIcon name="undraw_online_ad" class="pr-4" />
+        </div>
         <div class="w-2/3 h-40">
           <h2 class="font-bold text-xl">Promote events</h2>
           <p>
@@ -103,6 +120,16 @@
             clicks with no emails and calls.
           </p>
         </div>
+        <div class="w-1/3">
+          <TIcon name="undraw_verified" class="pl-4" />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-8 bg-black text-white text-sm">
+      <div class="p-4 container mx-auto ">
+        <p>Connect with us and hit up #wedance</p>
+        <p>© WeDance 2020</p>
       </div>
     </div>
   </div>
