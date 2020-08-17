835094cc3ebb3a92c35ce98ad97b9701ef1e63ed
Aleksey Razbakov <aleksey@razbakov.com>
2020-08-17 14:12:32 +0200

    landing for city

---
+++ pages/_city.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="font-sans leading-normal tracking-normal antialiased">
+    <header class="container mx-auto bg-white">
+      <div class="flex items-center justify-between flex-wrap p-4">
+        <div class="flex items-center justify-start">
+          <router-link to="/" class="flex items-center">
+            <TIcon class="w-6 h-6 mt-2 mr-2" name="icon" />
+            <TIcon class="h-4 w-32" name="logo-text" />
+          </router-link>
+        </div>
+        <TButton class="mt-4" type="secondary" to="/signin">Log in</TButton>
+      </div>
+    </header>
+
+    <div class="bg-orange-100">
+      <div class="text-gray-900 py-20 px-4 mx-auto max-w-2xl">
+        <h1 class="text-4xl font-serif leading-tight">
+          Dance community in
+          <div class="inline-block">
+            <TInputSelect
+              :options="cityList"
+              :value="citySelected"
+              @input="(city) => $router.push(`/${city}`)"
+            />
+          </div>
+        </h1>
+        <p class="mt-2 text-xl leading-tight">
+          Connect with dancers and plan activities easier. Salsa, Bachata,
+          Kizomba, Zouk and more!
+        </p>
+        <div class="md:flex text-center">
+          <TButton class="mt-4 md:mr-4" type="primary" to="/signin"
+            >Join community</TButton
+          >
+          <TButton
+            class="mt-4"
+            type="secondary"
+            href="https://www.notion.so/WeDance-d8a81e56732e4c10958073a8b887a673"
+            >About WeDance</TButton
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="p-4 max-w-2xl mx-auto">
+      <div class="mt-4 flex min-w-full justify-end">
+        <div class="w-2/3 h-40">
+          <h2 class="font-bold text-xl">Start dancing</h2>
+          <p>
+            Discover all dance styles in your city, find your passion, get
+            inspired and plan your first workshop.
+          </p>
+        </div>
+      </div>
+      <div class="mt-4 flex min-w-full justify-start">
+        <div class="w-2/3 h-40">
+          <h2 class="font-bold text-xl">Find dance partner</h2>
+          <p>
+            Let us know who you are looking for and we will recommend you
+            someone.
+          </p>
+        </div>
+      </div>
+      <div class="mt-4 flex min-w-full justify-end">
+        <div class="w-2/3 h-40">
+          <h2 class="font-bold text-xl">Find parties</h2>
+          <p>
+            We monitor all activities in dance scene and provide you with event
+            calendar, which will highlight the best party for you.
+          </p>
+        </div>
+      </div>
+      <div class="mt-4 flex min-w-full justify-start">
+        <div class="w-2/3 h-40">
+          <h2 class="font-bold text-xl">Improve dancing</h2>
+          <p>
+            Find and connect with dance instructors, dance schools and
+            festivals.
+          </p>
+        </div>
+      </div>
+
+      <div class="mt-4 flex min-w-full justify-end">
+        <div class="w-2/3 h-40">
+          <h2 class="font-bold text-xl">Promote events</h2>
+          <p>
+            Organising a party with us is easier and faster. We help you to
+            connect with dancers and collaborate with other organisers.
+          </p>
+        </div>
+      </div>
+      <div class="mt-4 flex min-w-full justify-start">
+        <div class="w-2/3 h-40">
+          <h2 class="font-bold text-xl">Book venues</h2>
+          <p>
+            Get access to our currated list of studios and open air places
+            available for parties or workshops. See availability. Book in few
+            clicks with no emails and calls.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostsIndex',
+  layout: 'empty',
+  computed: {
+    cityList() {
+      return [
+        {
+          value: '',
+          label: 'Your city'
+        },
+        {
+          value: 'Munich',
+          label: 'Munich'
+        },
+        {
+          value: 'new',
+          label: 'New city'
+        }
+      ]
+    },
+    citySelected() {
+      return this.$route.params.city
+    }
+  }
+}
+</script>
