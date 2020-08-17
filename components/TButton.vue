05d7a04fb416a20b9c61cc8775084b6fa12c4a4d
Aleksey Razbakov <aleksey@razbakov.com>
2020-08-17 23:54:02 +0200

    update colors

--- components/TButton.vue
+++ components/TButton.vue
@@ -71,13 +71,13 @@ export default {
         simple:
           'bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow',
         primary:
-          'bg-blue-500 no-underline text-white font-semibold py-2 px-4 rounded hover:text-white hover:bg-black shadow',
+          'bg-primary no-underline text-white font-semibold py-2 px-4 rounded hover:text-white hover:bg-black shadow',
         danger:
           'bg-red-500 no-underline text-white font-semibold py-2 px-4 rounded hover:text-white hover:bg-red-700 shadow',
         base:
           'bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow',
         secondary:
-          'bg-transparent text-blue-500 font-semibold py-2 px-4 border border-blue-500 rounded no-underline hover:bg-blue-500 hover:text-white hover:border-transparent',
+          'bg-transparent text-primary font-semibold py-2 px-4 border border-primary rounded no-underline hover:bg-primary hover:text-white hover:border-transparent',
         link: 'underline font-semibold hover:no-underline',
         nav:
           'block text-left w-full px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white',
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EA1F59',
+        secondary: '#FA9169',
+        light: '#F1F692',
+        dark: '#3C3348',
+        black: '#2A1B3C',
+        white: '#FFFFF4'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
