23feff27370207510c5f508b78dc8d86b657ec88
Aleksey Razbakov <aleksey@razbakov.com>
2020-09-28 17:45:50 +0200

    fix chat button not shown

--- use/cities.js
+++ use/cities.js
@@ -12,6 +12,8 @@ export default () => {
 
   const currentCity = computed(() => state.currentCity)
 
+  find('name', currentCity.value)
+
   watch(currentCity, (currentCity) => {
     if (currentCity) {
       ls('city', currentCity)
