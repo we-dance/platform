878606121f69faa76c6c74ab0598f393618b2888
Aleksey Razbakov <aleksey@razbakov.com>
2020-10-14 20:04:23 +0200

    debug in-app browsers

---
+++ pages/debug.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="border rounded p-4">
+      <div>Browser: {{ browser }}</div>
+      <div>In App: {{ isInApp ? 'Yes' : 'No' }}</div>
+    </div>
+
+    <div class="mt-4"><a :href="link" target="_blank">Open in Browser</a></div>
+  </div>
+</template>
+
+<script>
+import InApp from 'detect-inapp'
+const inapp = new InApp(navigator.userAgent || navigator.vendor || window.opera)
+
+export default {
+  computed: {
+    link() {
+      return location.href
+    },
+    browser() {
+      return inapp.browser
+    },
+    isInApp() {
+      return inapp.isInApp
+    }
+  }
+}
+</script>
