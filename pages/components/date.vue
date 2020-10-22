c7e8396bfe09c678d8ec2ad20648ac9e369686ef
Aleksey Razbakov <aleksey@razbakov.com>
2020-10-22 16:06:35 +0200

    fix event editor in safari

---
+++ pages/components/date.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <pre>Selected date is - {{ date }}</pre>
+    <div class="mt-4">
+      <TInputDateTime v-model="date" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    date: ''
+  })
+}
+</script>
