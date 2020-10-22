dd29993dca86ba2bf95ac7aabbf503a708efb1db
Aleksey Razbakov <aleksey@razbakov.com>
2020-10-22 16:19:50 +0200

    fix

--- components/TResizable.vue
+++ components/TResizable.vue
@@ -1,16 +1,16 @@
 <script>
 export default {
   name: 'ResizeAuto',
+  mounted() {
+    this.$nextTick(() => {
+      this.$el.setAttribute('style', 'height', `${this.$el.scrollHeight}px`)
+    })
+  },
   methods: {
     resize(event) {
       event.target.style.height = 'auto'
       event.target.style.height = `${event.target.scrollHeight}px`
     }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.$el.setAttribute('style', 'height', `${this.$el.scrollHeight}px`)
-    })
   },
   render() {
     return this.$scopedSlots.default({
