ff1e7979f3a807315219482af56f842be2de676d
Aleksey Razbakov <aleksey@razbakov.com>
2020-10-27 16:52:55 +0100

    use styles from yml (#4)

---
+++ sourcy.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sources: [
+    {
+      spreadsheetId: '1oCLW_c_Jr021AT6_gGngtFB94P_5ftSCJGp-XqdvRaM',
+      range: 'Styles',
+      output: './content/styles',
+      format: 'yaml'
+    }
+  ]
+}
