55f3134f5af4b7c162cd4d9b789849710e52b08e
Aleksey Razbakov <aleksey@razbakov.com>
2021-02-17 11:23:52 +0100

    add event types

--- use/events.js
+++ use/events.js
@@ -1,12 +1,32 @@
 export default () => {
   const eventTypeList = [
     {
-      label: 'Course',
-      value: 'Course'
+      label: 'Workshop',
+      value: 'Workshop'
     },
     {
       label: 'Party',
       value: 'Party'
+    },
+    {
+      label: 'Festival',
+      value: 'Festival'
+    },
+    {
+      label: 'Congress',
+      value: 'Congress'
+    },
+    {
+      label: 'Concert',
+      value: 'Concert'
+    },
+    {
+      label: 'Show',
+      value: 'Show'
+    },
+    {
+      label: 'Other',
+      value: 'Other'
     }
   ]
 
