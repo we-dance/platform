99ca391ef670f5ab77f3513be6d84c4940087cb0
Aleksey Razbakov <aleksey@razbakov.com>
2021-02-18 16:45:15 +0100

    identify hubspot chat

--- use/auth.js
+++ use/auth.js
@@ -202,6 +202,17 @@ export default () => {
         pwaUsed
       })
 
+    const _hsq = (window._hsq = window._hsq || [])
+    _hsq.push([
+      'identify',
+      {
+        email: state.account.email,
+        id: state.uid,
+        firstName: state.profile.name,
+        username: state.profile.username
+      }
+    ])
+
     if (!state.account.marketing) {
       await firestore
         .collection('accounts')
