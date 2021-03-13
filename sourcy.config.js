95a8c3c8b8d8932862aa6d7607712c7b468d8fec
Aleksey Razbakov <aleksey@razbakov.com>
2021-03-13 17:22:26 +0100

    content: sourcy config

--- sourcy.config.js
+++ sourcy.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   sources: [
-    // {
-    //   spreadsheetId: '1oCLW_c_Jr021AT6_gGngtFB94P_5ftSCJGp-XqdvRaM',
-    //   range: 'Styles',
-    //   output: './content/styles',
-    //   format: 'yaml'
-    // },
+    {
+      spreadsheetId: '1oCLW_c_Jr021AT6_gGngtFB94P_5ftSCJGp-XqdvRaM',
+      range: 'Styles',
+      output: './content/styles',
+      format: 'yaml'
+    },
     {
       spreadsheetId: '1bYfvOamLMvJH461xYYXoBYKqqFpPsAHVXF90kzy3CFg',
       range: 'Langs',
