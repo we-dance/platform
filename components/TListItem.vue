608f1a5ff9a36640a3bb279fad7ff695b9967f43
Aleksey Razbakov <aleksey@razbakov.com>
2021-03-20 15:56:40 +0100

    fix signup city [ch181]

---
+++ components/TListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-4">
+    <dl v-for="field in fields" :key="field.value" class="flex space-x-2">
+      <dt class="font-bold w-1/3 text-right">{{ field.label }}</dt>
+      <dd>{{ item[field.value] }}</dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TListItem',
+  props: {
+    item: {
+      type: Object,
+      default: () => ({})
+    },
+    fields: {
+      type: Array,
+      default: () => ['id']
+    }
+  }
+}
+</script>
---
+++ pages/admin/suspended.vue
@@ -0,0 +1,23 @@
+<template>
+  <TCardList v-bind="config" />
+</template>
+
+<script>
+import { getOptionsFromArray } from '~/utils'
+
+export default {
+  name: 'SuspendedIndex',
+  setup() {
+    const config = {
+      collection: 'suspended',
+      title: 'Suspended',
+      empty: 'Nothing here',
+      fields: getOptionsFromArray(['username', 'reason', 'email', 'deletedAt'])
+    }
+
+    return {
+      config
+    }
+  }
+}
+</script>
