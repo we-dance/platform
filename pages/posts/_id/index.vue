2b84caa817ddc69a39f3b62a25b2eae31751f605
Aleksey Razbakov <aleksey@razbakov.com>
2021-04-13 00:29:33 +0200

    hide post for guests

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -14,16 +14,47 @@
         align="center"
         class="md:-mt-4 md:-mx-4"
       />
-      <TPreview :content="doc.description" class="mt-4" />
+      <TPreview
+        :content="doc.description"
+        class="mt-4"
+        :class="uid ? '' : 'h-64 overflow-hidden'"
+      />
+      <div v-if="!uid" class="p-4 text-center bg-gray-100 rounded">
+        <router-link
+          to="/signin"
+          class="underline text-blue-500 hover:no-underline"
+          >Login</router-link
+        >
+        to read full post. Not a member yet?
+        <router-link
+          to="/signin"
+          class="underline text-blue-500 hover:no-underline"
+          >Register</router-link
+        >.
+      </div>
       <TItemFooter collection="posts" :item="doc" :title="doc.title" />
       <TItemCreator :item="doc" />
     </TItemCard>
 
-    <TItemComments :reply-to="doc.createdBy" :post-id="doc.id" />
+    <TItemComments v-if="uid" :reply-to="doc.createdBy" :post-id="doc.id" />
+    <div v-else class="mt-4 p-4 text-center bg-gray-100 rounded">
+      <router-link
+        to="/login"
+        class="underline text-blue-500 hover:no-underline"
+        >Login</router-link
+      >
+      to see discussion. Not a member yet?
+      <router-link
+        to="/register"
+        class="underline text-blue-500 hover:no-underline"
+        >Register</router-link
+      >.
+    </div>
   </div>
 </template>
 
 <script>
+import { useAuth } from '~/use/auth'
 import { useProfiles } from '~/use/profiles'
 import { getExcerpt, getMeta, loadDoc } from '~/utils'
 
@@ -40,9 +71,11 @@ export default {
     return getMeta('posts', this.doc)
   },
   setup() {
+    const { uid } = useAuth()
     const { getProfile } = useProfiles()
 
     return {
+      uid,
       getProfile,
       getExcerpt
     }
