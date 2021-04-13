3a53ca6e30f73b7e91a9dd0686dd60e42c39ffbb
Aleksey Razbakov <aleksey@razbakov.com>
2021-04-14 00:04:52 +0200

    add w-embedso widget

---
+++ components/global/WEmbedso.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    position: {
+      type: String,
+      default: 'right'
+    }
+  },
+  mounted() {
+    window.EMBED_SO_CONFIG = {
+      id: this.id,
+      position: this.position
+    }
+
+    const script = document.createElement('script')
+    script.setAttribute('src', 'https://embed.so/js/embed.js')
+    script.async = true
+
+    document.head.appendChild(script)
+  }
+}
+</script>
