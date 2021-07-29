8e343d2619d3063480dbe05ea07d83a75238dfec
amanuela97 <babyzewdie@gmail.com>
2021-07-29 18:49:37 +0300

    Errors in console during registration fixed

--- pages/register.vue
+++ pages/register.vue
@@ -99,7 +99,7 @@ export default {
       this.$router.push(`/${this.profile.username}`)
     },
     async submit({ email, username, password, place }) {
-      if (!email.trim() || !username.trim() || !password.trim()) {
+      if (!email?.trim() || !username?.trim() || !password?.trim()) {
         this.error = {
           message: 'Please fill all the fields'
         }
