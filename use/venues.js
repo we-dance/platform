f2e898a0265010e7e10acdddded78bd99ec757a9
Aleksey Razbakov <aleksey@razbakov.com>
2021-08-08 16:22:54 +0200

    add field type: venues

---
+++ use/venues.js
@@ -0,0 +1,28 @@
+export const venueFields = [
+  {
+    name: 'name',
+    label: 'Name'
+  },
+  {
+    name: 'address',
+    label: 'Address',
+    type: 'address'
+  },
+  {
+    name: 'place',
+    label: 'City',
+    type: 'place'
+  },
+  {
+    name: 'phone',
+    label: 'Phone'
+  },
+  {
+    name: 'website',
+    label: 'Website'
+  },
+  {
+    name: 'email',
+    label: 'Email'
+  }
+]
