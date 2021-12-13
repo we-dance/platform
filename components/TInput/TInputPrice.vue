0322d95598a51635c44c7a2a66e027a19ad97435
Recep Öztürk <recep.fed@gmail.com>
2021-12-13 21:29:32 +0300

    fix lint

--- components/TInput/TInputPrice.vue
+++ components/TInput/TInputPrice.vue
@@ -4,7 +4,7 @@
       ref="input"
       v-model="amount"
       v-bind="$attrs"
-      class="text-black placeholder-gray-400 bg-white border-gray-300 focus:border-blue-500  block text-sm w-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+      class="text-black placeholder-gray-400 bg-white border-gray-300 focus:border-blue-500 block text-sm w-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
     />
     <TInputSelect v-model="currency" :options="options" class="border-2" />
   </div>
