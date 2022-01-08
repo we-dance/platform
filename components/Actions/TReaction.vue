64845abc75b9c79383f4f76d046965aa4b9d167c
Aleksey Razbakov <aleksey@razbakov.com>
2022-01-08 17:50:40 +0100

    reaction button group

--- components/Actions/TReaction.vue
+++ components/Actions/TReaction.vue
@@ -1,17 +1,27 @@
 <template>
-  <TButton
-    type="xs"
-    :class="clicked ? 'font-bold' : ''"
-    :title="label"
-    :data-names="names"
-    @click="toggle"
+  <div
+    class="divide-x justify-center inline-flex items-center rounded border border-gray-300 overflow-hidden"
   >
-    <component :is="icon" class="w-4 h-4" />
-    <div class="ml-1">{{ clicked ? toggledLabel : label }}</div>
-    <div class="ml-1 text-xs rounded-full bg-gray-200 px-1 block">
-      {{ count }}
-    </div>
-  </TButton>
+    <button
+      class="flex flex-shrink-0 px-2 py-1 shadow-sm text-xs font-medium text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      :class="clicked ? 'font-bold' : ''"
+      :title="label"
+      :data-names="names"
+      @click="toggle"
+    >
+      <component :is="icon" class="w-4 h-4" />
+      <div class="ml-1">{{ clicked ? toggledLabel : label }}</div>
+      <div class="ml-1 text-xs rounded-full bg-gray-200 px-1 block">
+        {{ count }}
+      </div>
+    </button>
+
+    <button
+      class="px-2 py-1 shadow-sm text-xs font-medium text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      <ChevronDownIcon class="w-4 h-4" />
+    </button>
+  </div>
 </template>
 <script>
 import firebase from 'firebase/app'
@@ -23,11 +33,13 @@ import {
   StarIcon,
   ArchiveIcon,
   EyeOffIcon,
+  ChevronDownIcon,
 } from '@vue-hero-icons/outline'
 import { computed } from 'vue-demi'
 
 export default {
   components: {
+    ChevronDownIcon,
     EyeIcon,
     StarIcon,
     ArchiveIcon,
---
+++ pages/demo/stars.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-4">
+    <div class="mb-4">
+      Actions
+    </div>
+    <div>
+      <TReaction
+        label="Star"
+        toggledLabel="Starred"
+        field="star"
+        icon="StarIcon"
+        :item="item"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
