1458b144b7c207e258ce5843658badb5150090e2
Aleksey Razbakov <aleksey@razbakov.com>
2022-01-25 00:17:27 +0100

    upgrade tailwind to v3

---
+++ pages/demo/form.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4">
+    <h1 class="font-bold">Form Example</h1>
+    <div class="relative">
+      <input
+        type="text"
+        id="email"
+        class="border px-4 py-2 peer"
+        placeholder="Email"
+      />
+      <label for="email" class="font-bold peer-placeholder-shown:uppercase"
+        >Email</label
+      >
+    </div>
+  </div>
+</template>
