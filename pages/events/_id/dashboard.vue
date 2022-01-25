ce860ec482ff0a75db525e676143863a27348267
Aleksey Razbakov <aleksey@razbakov.com>
2022-01-25 12:03:36 +0100

    chore: i18n form fields (#93)

--- pages/events/_id/dashboard.vue
+++ pages/events/_id/dashboard.vue
@@ -275,7 +275,7 @@ import { useRsvp } from '~/use/rsvp'
 import { useRouter } from '~/use/router'
 import { useProfiles } from '~/use/profiles'
 import { useReactions } from '~/use/reactions'
-import { accountFields } from '~/use/accounts'
+import { useAccounts } from '~/use/accounts'
 import { getDateTime, getDate, getTime, dateDiff, sortBy } from '~/utils'
 
 export default {
@@ -286,6 +286,7 @@ export default {
     const { uid, can, account } = useAuth()
     const { params } = useRouter()
     const { getProfile } = useProfiles()
+    const { accountFields } = useAccounts()
 
     const { doc, load, exists, loading } = useDoc('posts')
     const { create } = useDoc('emails')
