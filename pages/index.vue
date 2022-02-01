1d68149b31a515e41fbe5a0a3677b0e783033d9a
Harsh Brahmxatriya <harshappy15@gmail.com>
2022-02-01 18:53:17 +0530

    change homepage title

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
               <p
                 class="mt-2 max-w-lg mx-auto text-center sm:max-w-3xl text-white"
               >
-                {{ $t('hero.motto') }}
+                Hello from Harsh B
               </p>
 
               <div class="max-w-lg mx-auto">
