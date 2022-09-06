821a4853f3dd107cba17773d5ca76754508bc5d8
mr.robot <gomexxxx71@gmail.com>
2022-09-06 03:17:54 -0600

    TInputEvent.vue created.

---
+++ components/TInput/TInputEvent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="!value.name">
+      <TButton>Add Event</TButton>
+    </div>
+    <div v-else>
+      <div>Event Editor</div>
+      <pre>{{ value }}</pre>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      value: {
+        type: Object,
+        default: () => ({}),
+      },
+      placeholder: {
+        type: String,
+        default: 'placeholder'
+      },
+    },
+  }
+  </script>
