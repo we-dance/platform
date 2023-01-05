28effc74cf45d62075dcac034808b55e37caccd1
Aleksey Razbakov <aleksey@razbakov.com>
2023-01-05 19:52:55 +0100

    docs: About WeDance

---
+++ docs/app.config.ts
@@ -0,0 +1,23 @@
+export default defineAppConfig({
+  docus: {
+    title: 'WeDance Docs',
+    description: 'The best place to start your documentation.',
+    image: 'https://wedance.vip/svg/logo-horizontal-dark.svg',
+    socials: {
+      twitter: 'wedanceplatform',
+      github: 'we-dance/platform',
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+    },
+    footer: {
+      iconLinks: [],
+    },
+  },
+})
