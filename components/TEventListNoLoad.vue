944dbe3db398ba0aa1961baa8d6268dbdf0634c2
Aleksey Razbakov <aleksey@razbakov.com>
2023-01-14 19:21:34 +0100

    calendar: fix add button

--- components/TEventListNoLoad.vue
+++ components/TEventListNoLoad.vue
@@ -35,11 +35,10 @@
     <div class="px-4">
       <h4 class="font-bold text-xl pb-4">{{ $t('calendar.title') }}</h4>
     </div>
-    <div class="flex justify-between items-center px-4">
+    <div class="flex justify-start items-center px-4">
+      <TButton to="/events/-/edit" type="primary" icon="plus" />
       <TInputButtons v-model="tab" :options="tabs" />
-      <TButton to="/events/-/edit" type="primary">{{
-        $t('myprofile.addEvent')
-      }}</TButton>
+      <div class="flex-grow"></div>
       <TButton type="nav" icon="share" @click="showPopup = true" />
     </div>
     <div class="space-y-8 mt-4">
