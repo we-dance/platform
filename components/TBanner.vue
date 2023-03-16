5874c0ab994ac55b4cca49fa57b87e42032874c2
Yullu <joshuamuuo5@gmail.com>
2023-03-16 15:42:07 +0300

    Issue "https://github.com/we-dance/platform/issues/344" fixed issue>>upload photo TBanner was opening  a new tab

--- components/TBanner.vue
+++ components/TBanner.vue
@@ -35,13 +35,23 @@
           class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto"
         >
           <a
+            v-if="link"
             :href="link"
-            target="_blank"
+            target=""
             class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             @click="$emit('input', 'yes')"
           >
             {{ action }}
           </a>
+          <NuxtLink
+            v-else-if="to"
+            target=""
+            class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            @click="$emit('input', 'yes')"
+            :to="to"
+          >
+            {{ action }}
+          </NuxtLink>
         </div>
         <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
           <button
@@ -93,6 +103,10 @@ export default {
       default: '',
     },
     link: {
+      type: String,
+      default: '',
+    },
+    to: {
       type: String,
       default: '',
     },
