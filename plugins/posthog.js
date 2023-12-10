d1a3da3a0a6e97f34059a4d9c3a94e0f39b611e6
Aleksey Razbakov <aleksey@razbakov.com>
2023-12-10 16:04:08 +0100

    fix posthog

--- plugins/posthog.js
+++ plugins/posthog.js
@@ -3,7 +3,6 @@ import posthog from 'posthog-js'
 export default function({ app: { router } }, inject) {
   posthog.init('phc_vL8Ex5Ph5yi1aoEALtthrjI0yC3w9vdyo05Acbdstqg', {
     api_host: 'https://app.posthog.com',
-    capture_pageview: false,
   })
 
   inject('posthog', posthog)
