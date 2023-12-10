c4a82c25124a434ba974a5df7cb7ffe189ca2ff5
Aleksey Razbakov <aleksey@razbakov.com>
2023-12-10 16:00:03 +0100

    fix posthog

--- plugins/posthog.js
+++ plugins/posthog.js
@@ -1,20 +1,10 @@
 import posthog from 'posthog-js'
-import Vue from 'vue'
 
 export default function({ app: { router } }, inject) {
   posthog.init('phc_vL8Ex5Ph5yi1aoEALtthrjI0yC3w9vdyo05Acbdstqg', {
     api_host: 'https://app.posthog.com',
     capture_pageview: false,
-    loaded: () => posthog.identify('unique_id'),
   })
 
   inject('posthog', posthog)
-
-  router.afterEach((to) => {
-    Vue.nextTick(() => {
-      posthog.capture('$pageview', {
-        $current_url: to.fullPath,
-      })
-    })
-  })
 }
