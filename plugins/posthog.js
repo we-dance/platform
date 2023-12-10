a35580a47f2060b381fda35ba29978ef80d92bc1
Aleksey Razbakov <aleksey@razbakov.com>
2023-12-10 16:16:48 +0100

    disable posthog locally

--- plugins/posthog.js
+++ plugins/posthog.js
@@ -1,9 +1,11 @@
 import posthog from 'posthog-js'
 
 export default function({ app: { router } }, inject) {
-  posthog.init('phc_vL8Ex5Ph5yi1aoEALtthrjI0yC3w9vdyo05Acbdstqg', {
-    api_host: 'https://app.posthog.com',
-  })
+  if (process.env.NODE_ENV === 'production') {
+    posthog.init('phc_vL8Ex5Ph5yi1aoEALtthrjI0yC3w9vdyo05Acbdstqg', {
+      api_host: 'https://app.posthog.com',
+    })
+  }
 
   inject('posthog', posthog)
 }
