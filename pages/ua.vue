3344acc4081102ad66d7611c69c8d43aa3ddc012
Aleksey Razbakov <aleksey@razbakov.com>
2024-01-29 20:36:10 +0100

    debug user agent

---
+++ pages/ua.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-4 font-mono">{{ ua }}</div>
+</template>
+
+<script>
+const ua = navigator.userAgent || navigator.vendor || window.opera
+
+export default {
+  computed: {
+    ua() {
+      return ua
+    },
+  },
+}
+</script>
