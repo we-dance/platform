d6db0146bf6c7ead55f38b2f301751a09f931b0a
Aleksey Razbakov <aleksey@razbakov.com>
2024-02-10 16:49:20 +0100

    profile: cleanup

--- components/TProfile.vue
+++ components/TProfile.vue
@@ -77,11 +77,7 @@
           :alt="profile.username"
           class="w-full rounded-full"
         />
-        <TIcon
-          v-else-if="profile.type !== 'City'"
-          name="undraw_profile_pic"
-          class="w-full rounded-full"
-        />
+        <TIcon v-else name="undraw_profile_pic" class="w-full rounded-full" />
       </div>
 
       <div class="col-span-3">
@@ -133,26 +129,17 @@
       />
     </div>
 
-    <WTeaser
-      v-if="profile.partner === 'Yes'"
-      :title="$t('profile.partnerSearch.title')"
-      :description="profile.partnerBio"
-      class="w-full mt-4"
+    <TLoader v-if="!loaded" class="py-4" />
+    <TEventListNoLoad
+      v-else
+      :community="profile.username"
+      :username="profile.username"
+      :docs="events"
+      class="w-full border-b border-t pt-4"
     />
 
-    <template v-if="!$route.query.beta">
-      <TLoader v-if="!loaded" class="py-4" />
-      <TEventListNoLoad
-        v-else
-        :community="profile.username"
-        :username="profile.username"
-        :docs="events"
-        class="w-full border-b border-t pt-4"
-      />
-    </template>
-
     <WTeaser
-      v-if="!uid && profile.type !== 'City' && profile.place"
+      v-if="!uid && profile.place"
       :title="$t('profile.invite.header', { count: invitesLeft })"
       :description="$t('profile.invite.description', { name: profile.name })"
       :button="$t('profile.invite.action')"
