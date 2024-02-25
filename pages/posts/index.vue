6828b10d827acbbbe044dc7d2c1e96e514c7a979
Aleksey Razbakov <aleksey@razbakov.com>
2024-02-25 16:16:51 +0100

    limit to 3 stories and posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -56,7 +56,7 @@ export default {
       collection = collection
         .where('type', '==', 'post')
         .orderBy('createdAt', 'desc')
-        .limit(10)
+        .limit(3)
 
       if (lastVisible.value) {
         collection = collection.startAfter(lastVisible.value)
