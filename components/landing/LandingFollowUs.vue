c45c6bf366804eece491993aee4a3ba02c31d7cb
Aleksey Razbakov <aleksey@razbakov.com>
2024-03-11 12:40:41 +0100

    update follow us icons

--- components/landing/LandingFollowUs.vue
+++ components/landing/LandingFollowUs.vue
@@ -5,43 +5,16 @@
       <h3 class="text-2xl font-extrabold text-center">
         Follow Us
       </h3>
-      <div class="p-4 flex flex-wrap gap-2 items-center justify-center">
-        <TButton
-          allow-guests
-          icon="youtube"
-          type="round"
-          icon-size="6"
-          href="https://www.youtube.com/channel/UC1pdWS3nCLrd8LnQyNNSNUw"
-        />
-        <TButton
-          allow-guests
-          icon="instagram"
-          type="round"
-          icon-size="6"
-          href="https://instagram.com/WeDancePlatform"
-        />
-        <TButton
-          allow-guests
-          icon="telegram"
-          type="round"
-          icon-size="6"
-          href="https://t.me/WeDancePlatform"
-        />
-        <TButton
-          allow-guests
-          icon="twitter"
-          type="round"
-          icon-size="6"
-          href="https://twitter.com/WeDancePlatform"
-        />
-        <TButton
-          allow-guests
-          icon="facebook"
-          type="round"
-          icon-size="6"
-          href="https://www.facebook.com/WeDance-109777921433083"
-        />
-      </div>
+      <TContactsGrid
+        class="mt-4"
+        :profile="{
+          youtube: 'https://www.youtube.com/channel/UC1pdWS3nCLrd8LnQyNNSNUw',
+          instagram: 'https://instagram.com/WeDancePlatform',
+          telegram: 'https://t.me/WeDancePlatform',
+          twitter: 'https://twitter.com/WeDancePlatform',
+          facebook: 'https://www.facebook.com/WeDance-109777921433083',
+        }"
+      />
     </div>
   </div>
 </template>
