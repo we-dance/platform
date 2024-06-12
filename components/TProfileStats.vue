d3b6540d72a206c423b98bdc1829a962bc419fa7
Aleksey Razbakov <aleksey@razbakov.com>
2024-06-12 10:31:22 +0200

    profile: show subscribers list

--- components/TProfileStats.vue
+++ components/TProfileStats.vue
@@ -1,25 +1,43 @@
 <template>
-  <div class="flex flex-wrap text-xs text-gray-500 space-x-1">
-    <template v-if="profile.type">
-      <TProfileType :type="profile.type" />
+  <div>
+    <div class="flex flex-wrap text-xs text-gray-500 space-x-1">
+      <template v-if="profile.type">
+        <TProfileType :type="profile.type" />
+        <div>·</div>
+      </template>
+      <template v-if="profile.venueType">
+        <TVenueType :type="profile.venueType" />
+        <div>·</div>
+      </template>
+      <div>
+        {{ $tc('views', profile.viewsCount, { count: profile.viewsCount }) }}
+      </div>
       <div>·</div>
-    </template>
-    <template v-if="profile.venueType">
-      <TVenueType :type="profile.venueType" />
-      <div>·</div>
-    </template>
-    <div>
-      {{ $tc('views', profile.viewsCount, { count: profile.viewsCount }) }}
+      <button @click="showSubscribers = true">
+        {{
+          $tc('subscribers', subscribersCount, {
+            count: subscribersCount,
+          })
+        }}
+      </button>
+      <TReviewCount :profile="profile" />
     </div>
-    <div>·</div>
-    <div>
-      {{
-        $tc('subscribers', subscribersCount, {
-          count: subscribersCount,
-        })
-      }}
-    </div>
-    <TReviewCount :profile="profile" />
+    <TPopup
+      v-if="showSubscribers"
+      :title="`${profile.username} subscribers`"
+      @close="showSubscribers = false"
+    >
+      <div class="py-6 overflow-y-scroll">
+        <div v-if="!profile.watch || profile.watch.usernames.length === 0">
+          <div class="text-center text-gray-500">
+            {{ $t('No subscribers') }}
+          </div>
+        </div>
+        <div v-else v-for="username in profile.watch.usernames" :key="username">
+          <WProfile :username="username" hide-buttons class="w-64" />
+        </div>
+      </div>
+    </TPopup>
   </div>
 </template>
 
@@ -31,6 +49,9 @@ export default {
       default: () => ({}),
     },
   },
+  data: () => ({
+    showSubscribers: false,
+  }),
   computed: {
     subscribersCount() {
       return (
