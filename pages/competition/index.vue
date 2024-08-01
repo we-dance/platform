4e6d4c97c0a5e491a01929c7b1a36ff1f6b1eac8
Aleksey Razbakov <aleksey@razbakov.com>
2024-08-01 12:55:34 +0200

    fix sponsor

--- pages/competition/index.vue
+++ pages/competition/index.vue
@@ -26,7 +26,7 @@
       </h2>
       <TPreview
         class="text-center"
-        content="@MontunoClub @la.kossa @kizztalkdanceacademy @the_social_dancer @marta @dancegodscompany.official @bachazouk_munich @Ernest @bavarian_bachata_congress"
+        content="@MontunoClub @lakossa @kizztalkdanceacademy @the_social_dancer @marta @dancegodscompany.official @bachazouk_munich @Ernest @bavarian_bachata_congress"
       />
     </div>
     <div class="p-4">
